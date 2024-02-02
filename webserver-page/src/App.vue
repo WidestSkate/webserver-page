70f0d71ea68d40f47c7a5202129b35420944ef0d
WidestSkate <20zamers-d@rondearingutc.com>
2024-02-02 20:41:27 +0000

    added basic functionality

--- webserver-page/src/App.vue
+++ webserver-page/src/App.vue
@@ -1,30 +1,11 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import year_groups from './components/year-groups.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1 class=" text-center mx-auto mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">RDUTC Year 13 websites list</h1>
+  <year_groups />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
---
+++ webserver-page/src/components/year-groups.vue
@@ -0,0 +1,43 @@
+<template>
+
+    <h1 class=" mt-24 mb-8 mx text-1xl font-extrabold leading-none tracking-tight md:text-2xl lg:text-3xl text-white">Class of 2024</h1>
+
+        <div class="lg:flex-cols-4 w-auto mt-3 lg:flex ml-3 mr-3 gap-5 flex-wrap flex-row sm:flex-cols-1 md:grid-cols-2 justify-evenly">
+
+            <page_panel v-for="name in names" :name="name" />
+
+        </div>
+</template>
+
+<script setup>
+
+import page_panel from './page-panel.vue'
+
+const names = [
+  "Alice",
+  "Bob",
+  "Charlie",
+  "David",
+  "Eva",
+  "Frank",
+  "Grace",
+  "Henry",
+  "Ivy",
+  "Jack",
+  "Katie",
+  "Liam",
+  "Mia",
+  "Noah",
+  "Olivia",
+  "Peter",
+  "Quinn",
+  "Rachel",
+  "Samuel",
+  "Taylor"
+];
+
+</script>
+
+<style scoped>
+/* Your component's CSS code goes here */
+</style>
