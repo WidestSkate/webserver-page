d006b3d55c79f172e71c12158ca735dc2a9ba6e3
WidestSkate <20zamers-d@rondearingutc.com>
2024-02-02 22:54:12 +0000

    added some test pages as well as changable data

--- webserver-page/src/App.vue
+++ webserver-page/src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import year_groups from './components/year-groups.vue'
+
 </script>
 
 <template>
   <h1 class=" text-center mx-auto mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">RDUTC Year 13 websites list</h1>
+  <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Created by Zakk Amers-Delph Ex Student</h5>
   <year_groups />
 </template>
 
--- webserver-page/src/components/year-groups.vue
+++ webserver-page/src/components/year-groups.vue
@@ -3,38 +3,38 @@
     <h1 class=" mt-24 mb-8 mx text-1xl font-extrabold leading-none tracking-tight md:text-2xl lg:text-3xl text-white">Class of 2024</h1>
 
         <div class="lg:flex-cols-4 w-auto mt-3 lg:flex ml-3 mr-3 gap-5 flex-wrap flex-row sm:flex-cols-1 md:grid-cols-2 justify-evenly">
-
-            <page_panel v-for="name in names" :name="name" />
-
+            <page_panel v-if="loaded" v-for="name in fileContent" :info="name" />
         </div>
 </template>
 
 <script setup>
 
 import page_panel from './page-panel.vue'
+import { onMounted, ref} from 'vue';
 
-const names = [
-  "Alice",
-  "Bob",
-  "Charlie",
-  "David",
-  "Eva",
-  "Frank",
-  "Grace",
-  "Henry",
-  "Ivy",
-  "Jack",
-  "Katie",
-  "Liam",
-  "Mia",
-  "Noah",
-  "Olivia",
-  "Peter",
-  "Quinn",
-  "Rachel",
-  "Samuel",
-  "Taylor"
-];
+var fileContent = null;
+var loaded = ref(false)
+
+onMounted(() => {
+    readTextFile('/data.txt');
+});
+
+async function readTextFile(filePath) {
+      try {
+        const response = await fetch(filePath);
+        if (!response.ok) {
+          throw new Error('Failed to fetch the file');
+        }
+        const content = await response.text();
+        fileContent = JSON.parse(content);
+        console.log(fileContent)
+        loaded.value = true
+        console.log(loaded)
+      } catch (error) {
+        console.error('Error reading the file:', error);
+      }
+    }
+
 
 </script>
 
