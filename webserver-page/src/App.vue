e49ae54d7417c7ef51ae02f2cceb1d1ed0974aa9
WidestSkate <20zamers-d@rondearingutc.com>
2024-02-03 14:17:16 +0000

    added logo and year group support

--- webserver-page/src/App.vue
+++ webserver-page/src/App.vue
@@ -1,12 +1,38 @@
 <script setup>
 import year_groups from './components/year-groups.vue'
+import { onMounted, ref} from 'vue';
 
+var fileContent = null;
+var loaded = ref(false)
+
+onMounted(() => {
+    readTextFile('/data.json');
+});
+
+async function readTextFile(filePath) {
+      try {
+        const response = await fetch(filePath);
+        if (!response.ok) {
+          throw new Error('Failed to fetch the file');
+        }
+        const content = await response.text();
+        fileContent = JSON.parse(content);
+        console.log(fileContent)
+        loaded.value = true
+        console.log(loaded)
+      } catch (error) {
+        console.error('Error reading the file:', error);
+      }
+    }
 </script>
 
 <template>
-  <h1 class=" text-center mx-auto mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">RDUTC Year 13 websites list</h1>
-  <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Created by Zakk Amers-Delph Ex Student</h5>
-  <year_groups />
+  <img class="h-auto w-80 mx-auto my-12" src="/logo.png" alt="Ron Dearing Logo">
+  <h1 class=" text-center mx-auto mb-12 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl ">RDUTC Year 13 websites list</h1>
+  <hr class="h-px mt-8 bg-gray-200 border-2 mx-auto dark:bg-gray-700 w-90">
+  <year_groups v-if="loaded" v-for="year in fileContent" :people="year"/>
+  <hr class="h-px my-8 bg-gray-200 border-1 mx-auto dark:bg-gray-700 w-full">
+  <h5 class="mb-8 text-sm font-bold tracking-tight text-gray-900 ">Created by Zakk Amers-Delph Ex Student</h5>
 </template>
 
 <style scoped>
--- webserver-page/src/components/year-groups.vue
+++ webserver-page/src/components/year-groups.vue
@@ -1,39 +1,17 @@
 <template>
 
-    <h1 class=" mt-24 mb-8 mx text-1xl font-extrabold leading-none tracking-tight md:text-2xl lg:text-3xl text-white">Class of 2024</h1>
-
+    <h1 class=" mt-24 mx text-1xl font-extrabold leading-none tracking-tight md:text-2xl lg:text-3xl text-black">Class of {{props.people[0]["year"]}}</h1>
+      <hr class="h-px mt-2 bg-gray-200 border-2 mx-auto dark:bg-gray-700 w-36 drop-shadow-2xl">
         <div class="lg:flex-cols-4 w-auto mt-3 lg:flex ml-3 mr-3 gap-5 flex-wrap flex-row sm:flex-cols-1 md:grid-cols-2 justify-evenly">
-            <page_panel v-if="loaded" v-for="name in fileContent" :info="name" />
+            <page_panel v-for="person in props.people" :info="person" />
         </div>
 </template>
 
 <script setup>
 
 import page_panel from './page-panel.vue'
-import { onMounted, ref} from 'vue';
 
-var fileContent = null;
-var loaded = ref(false)
-
-onMounted(() => {
-    readTextFile('/data.txt');
-});
-
-async function readTextFile(filePath) {
-      try {
-        const response = await fetch(filePath);
-        if (!response.ok) {
-          throw new Error('Failed to fetch the file');
-        }
-        const content = await response.text();
-        fileContent = JSON.parse(content);
-        console.log(fileContent)
-        loaded.value = true
-        console.log(loaded)
-      } catch (error) {
-        console.error('Error reading the file:', error);
-      }
-    }
+const props = defineProps(['people'])
 
 
 </script>
